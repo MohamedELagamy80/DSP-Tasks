d4cdec425676a99628aa6c039f8bc46d65d3c1f6
MohamedELagamy80 <77195046+MohamedELagamy80@users.noreply.github.com>
2021-10-30 12:26:15 +0200

    Add files via upload

---
+++ Tasks 1.m
@@ -0,0 +1,77 @@
+A=[-7 5 -9;2 -1 2;1 -1 2];
+B=[16 3 2 13;5 10 11 8;9 6 7 12;4 15 14 1];
+C=[4 2 -3 ;7 -7 9;3 -5 6];
+D= [6 3 2;2 12 -7;-1 6 2;-5 15 11];
+        % Task 1:
+%  1.1
+        3A-5C
+        
+
+  
+% 1.2 
+        7A+2B
+        %An Error Because matrix (A) is 3*3 and (B) is (4*4)
+        
+%1.3 
+      CA
+       
+ 
+  %1.4 
+          CD'
+          
+          
+          
+           %Task 2
+           %%  
+           
+ %2.1
+   zeros (3)
+           
+%  2.2
+zeros(3,4)
+     
+% 2.3
+ones(2)
+
+    
+%2.4 ones
+     (2,2)
+           
+           
+    %2.5 
+    size(D)
+    
+   %2.6 
+   zero(size(D))
+          
+%2.7 
+diag([1 2 3 4])
+   
+%2.8 
+eye(2)
+     
+     
+     %% 
+            %   Task 3
+            %3.1 
+            [A,B]
+     %A horzcat Error Because matrix (A) is 3*3 and (B) is (4*4)
+     
+     %3.2  
+         [A;B]
+           % A vertcat Error Because matrix (A) is 3*3 and (B) is (4*4)
+           
+           
+       %    Task 4
+       %% 
+       
+       s=diag([5 5 5 5 5 5 5 ]);
+       s(:,8)= [5;5;5;5;5;5;5]
+       
+       %   Task 5
+       %% 
+%        5.1 
+    A(2,:)
+ % 5.2 
+ A(:,3)
+
